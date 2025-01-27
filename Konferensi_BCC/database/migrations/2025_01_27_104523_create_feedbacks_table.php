6dfc0169aeff1bab1a043d2fc31dea177fdf8e64
muktiabdii <abdisyukur10@gmail.com>
2025-01-27 17:49:45 +0700

    Menambahkan tabel feedbacks melalui migration

---
+++ Konferensi_BCC/database/migrations/2025_01_27_104523_create_feedbacks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('feedbacks', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('commenter');
+            $table->unsignedBigInteger('session_id');
+            $table->text('feedback');
+            $table->timestamps();
+
+            $table->foreign('commenter')->references('id')->on('users');
+            $table->foreign('session_id')->references('id')->on('sessions');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('feedbacks');
+    }
+};
