3c843c51445cfb5c198614139bb8b18c071eb187
muktiabdii <abdisyukur10@gmail.com>
2025-01-27 17:52:27 +0700

    Menambahkan tabel proposals melalui migration

---
+++ Konferensi_BCC/database/migrations/2025_01_27_104849_create_proposals_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('proposals', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('author');
+            $table->string('title');
+            $table->text('description');
+            $table->enum('status', ['pending', 'accepted', 'rejected'])->default('pending');
+            $table->timestamps();
+
+            $table->foreign('author')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('proposals');
+    }
+};
