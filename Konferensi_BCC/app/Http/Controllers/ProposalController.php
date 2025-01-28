872692779405c6bb0931dbee09cf6ccf8a478abd
muktiabdii <abdisyukur10@gmail.com>
2025-01-28 20:11:15 +0700

    Menambahkan fitur membuat proposal

---
+++ Konferensi_BCC/app/Http/Controllers/ProposalController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Proposal;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Resources\ProposalResource;
+
+class ProposalController extends Controller
+{
+    public function create(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+        ]);
+
+        $proposal = Proposal::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'author' => Auth::id(),
+            'status' => 'pending'
+        ]);
+
+        return response()->json([
+            'message' => 'Proposal created succesfully',
+            'proposal' => new ProposalResource($proposal),
+        ], 201);
+    }
+}
--- Konferensi_BCC/routes/api.php
+++ Konferensi_BCC/routes/api.php
@@ -2,10 +2,11 @@
 
 use GuzzleHttp\Middleware;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\SessionRole;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\SessionController;
+use App\Http\Controllers\ProposalController;
 use App\Http\Controllers\AuthenticationController;
 
 Route::post('/register', [AuthenticationController::class, 'register']);
@@ -19,3 +20,6 @@ Route::get('session', [SessionController::class, 'index'])->middleware(['auth:sa
 Route::get('session/{id}', [SessionController::class, 'detail'])->middleware(['auth:sanctum', SessionRole::class]);
 Route::put('session/{id}', [SessionController::class, 'update'])->middleware(['auth:sanctum', SessionRole::class]);
 Route::delete('session/{id}', [SessionController::class, 'delete'])->middleware(['auth:sanctum', SessionRole::class]);
+
+
+Route::post('/proposal', [ProposalController::class, 'create'])->middleware('auth:sanctum');
